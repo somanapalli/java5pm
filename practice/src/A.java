d0b633a65d78af930bbb54a56657d37f8eba2d60
Somanapalli <rams.11mca@gmail.com>
2024-09-12 17:55:17 +0530

    commited

---
+++ practice/src/A.java
@@ -0,0 +1,8 @@
+//parent class
+public class A {
+
+	public void sum(int x, int y)
+	{
+		System.out.println(x+y);
+	}
+}
---
+++ practice/src/B.java
@@ -0,0 +1,30 @@
+//child class
+public class B extends A{
+	
+	public void sub(int x, int y)
+	{
+		System.out.println(x-y);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+//		B obj =new B();
+//		obj.sum(5, 6);
+//		obj.sub(5, 4);
+		
+		//A obj = new A();
+		//obj.sum(4, 5);
+		//obj.sub(6,4);
+		
+		//case3
+		//A obj = new B();
+		//obj.sum(4, 5);
+		//obj.sub(7,6);
+		
+		//case-4
+		//B obj = new A();
+		
+		
+	}
+}
